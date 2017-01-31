974021f1c657783310217bc9bbce27496c057f05
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2017-01-31 15:39:16 +0100

    Massive pruning of files :) <20kloc

--- src/Random.hpp
+++ src/Random.hpp
@@ -30,40 +30,40 @@ static const double Logroot2pi =0.918938533204673;
 
 class Random {
 
-	public:
+public:
 
-	static const double INFPROB;
+  static const double INFPROB;
 
-  	Random(int seed = -1);
+  Random(int seed = -1);
 
-	static void InitRandom(int seed = -1);
+  static void InitRandom(int seed = -1);
 
-	static int GetSeed();
+  static int GetSeed();
 
-	static double Uniform();
-	static int ApproxBinomial(int N, double p);
-	static int Poisson(double mu);
-	static double Gamma(double alpha,double beta);
-  	static double sNormal(void);
-  	static double sExpo(void);
-  	static double sGamma(double);
-  	static double sGammanew(double);
+  static double Uniform();
+  static int ApproxBinomial(int N, double p);
+  static int Poisson(double mu);
+  static double Gamma(double alpha,double beta);
+  static double sNormal(void);
+  static double sExpo(void);
+  static double sGamma(double);
+  static double sGammanew(double);
 
- 	static int Choose(int);
-	static int FiniteDiscrete(int n, const double* probarray);
-  	static void DrawFromUrn(int*, int n, int N);
-	static int DrawFromDiscreteDistribution(const double* p, int n);
+  static int Choose(int);
+  static int FiniteDiscrete(int n, const double* probarray);
+  static void DrawFromUrn(int*, int n, int N);
+  static int DrawFromDiscreteDistribution(const double* p, int n);
 
-	static double logGamma(double a);
+  static double logGamma(double a);
 
-	static double logMultivariateGamma(double a, int p);
-	private:
+  static double logMultivariateGamma(double a, int p);
+private:
 
-	static int Seed;
-	static int mt_index;
-	static unsigned long long mt_buffer[MT_LEN];
+  static int Seed;
+  static int mt_index;
+  static unsigned long long mt_buffer[MT_LEN];
 
 
-  };
+};
 
 #endif // RANDOM_H
