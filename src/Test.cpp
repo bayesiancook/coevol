a98cfeeb7e2cf827de1b22a2bff9186e1d6823c2
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2017-03-13 17:17:49 +0100

    Very basic simple move reimplementation.

--- src/Test.cpp
+++ src/Test.cpp
@@ -4,27 +4,35 @@
 #include "core/RandomTypes.hpp"
 using namespace std;
 
-class MySimpleMove : public MCUpdate {};
+class MySimpleMove : public MCUpdate {
+    MCMC* managedNode;
+public:
+    MySimpleMove(MCMC* managedNode): managedNode(managedNode) {}
+
+    double Move(double) override { // decided to ignore tuning modulator (ie, assume = 1)
+        return managedNode->Move(1.0);
+    }
+};
 
 class MyModel : public ProbModel {
   public:
-    Const<PosReal>* One;
+    Const<PosReal>* one;
     Beta* p;
     list<Binomial> leaves;
 
-    MyModel() : One(new Const<PosReal>(1)), p(new Beta(One, One)) {
+    MyModel() : one(new Const<PosReal>(1)), p(new Beta(one, one)) {
         for (int i=0; i<5; i++) {
             leaves.emplace_back(1, p);
             leaves.back().ClampAt(i<3?1:0);
         }
-        RootRegister(One);
+        RootRegister(one);
         Register();
         Update();
         MakeScheduler();
         getDot();
     }
 
-    void MakeScheduler() override { scheduler.Register(new SimpleMove(p, 1.0), 1, "p"); }
+    void MakeScheduler() override { scheduler.Register(new MySimpleMove(p), 1, "p"); }
 
     double GetLogProb() override { return p->GetLogProb(); }
 
