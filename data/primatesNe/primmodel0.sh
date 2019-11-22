29a8b7ecea88eb7d497d7f21d1cd9a213089a165
Nicolas Lartillot <nicolas.lartillot@gmail.com>
2019-11-22 16:48:39 +0100

    removed submission files

--- data/primatesNe/primmodel0.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --time=24:00:00
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --mem=1gb
-#SBATCH -o primmodel0.out
-#SBATCH -e primmodel0.err
-
-../coevol -f -d prim.phy -t prim.rootedtree -cal prim.calib 80 80 -c prim.lht -dsom primmodel0
