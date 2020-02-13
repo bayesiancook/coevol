b7e4e7bccfa28c95820fc5b39a85811cf8cdb7d8
Nicolas Lartillot <nicolas.lartillot@gmail.com>
2020-02-13 19:37:52 +0100

    removed old MPI struct Propagate_arg

--- sources/BranchOmegaMultivariate.cpp
+++ sources/BranchOmegaMultivariate.cpp
@@ -7,8 +7,6 @@
 
 // int BGCMutSelSubMatrix::bgccount = 0;
 
-MPI_Datatype Propagate_arg;
-
 class BranchOmegaMultivariateChain : public Chain	{
 
 	private:
@@ -411,16 +409,6 @@ int main(int argc, char* argv[])	{
 	MPI_Init(&argc,&argv);
 	MPI_Comm_rank(MPI_COMM_WORLD,&myid);
 	MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
-
-	int blockcounts[2] = {1,3};
-	MPI_Datatype types[2] = {MPI_DOUBLE,MPI_INT};
-	MPI_Aint dtex,displacements[2];
-
-	displacements[0] = (MPI_Aint) 0;
-	MPI_Type_extent(MPI_DOUBLE,&dtex);
-	displacements[1] = dtex;
-	MPI_Type_struct(2,blockcounts,displacements,types,&Propagate_arg);
-	MPI_Type_commit(&Propagate_arg);
 
 	if (! myid)	{
 		cerr << "Coevol version 1.5\n";
