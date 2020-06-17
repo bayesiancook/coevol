92ae66801647775b52e07e1a4f52e76bd90fdf23
Nicolas Lartillot <nicolas.lartillot@gmail.com>
2020-06-17 23:11:07 +0200

    coevol: reintroduced missing MPI initialization of myid and nproc

--- sources/BranchOmegaMultivariate.cpp
+++ sources/BranchOmegaMultivariate.cpp
@@ -410,6 +410,10 @@ int main(int argc, char* argv[])	{
 
 	int myid,nprocs;
 
+	MPI_Init(&argc,&argv);
+	MPI_Comm_rank(MPI_COMM_WORLD,&myid);
+	MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
+
 	if (! myid)	{
 		cerr << "Coevol version 1.5\n";
 		cerr << '\n';
