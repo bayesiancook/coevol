7cb9f3985acb2c012dbde04d11e901c4b95613bb
bayesiancook <nicolas.lartillot@gmail.com>
2021-05-11 00:32:37 +0200

    stripped down source code

--- sources/Heights.cpp
+++
@@ -1,4 +0,0 @@
-#include "Tree.h"
-
-
-
--- sources/MissingFractionPerTaxon.cpp
+++
@@ -1,9 +0,0 @@
-#include "SequenceAlignment.h"
-#include "Random.h"
-
-int main(int argc, char* argv[])	{
-
-	FileSequenceAlignment* data = new FileSequenceAlignment(argv[1]);
-    data->MissingFractionPerTaxon(cout);
-}
-
